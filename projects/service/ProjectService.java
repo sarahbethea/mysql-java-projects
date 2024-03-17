3769e5d060fb6ab157207fdd25b4c531146cfb15
Sarah Bethea <sarahbethea@sarahs-mbp.lan>
2024-03-17 13:06:10 -0600

    week 10

---
+++ projects/service/ProjectService.java
@@ -0,0 +1,45 @@
+package projects.service;
+
+import java.util.List;
+import java.util.NoSuchElementException;
+import java.util.Optional;
+
+import projects.dao.ProjectDao;
+import projects.entity.Project;
+import projects.exception.DbException;
+
+public class ProjectService {
+	
+	private ProjectDao projectDao = new ProjectDao();
+	
+	
+	
+	
+
+	public Project addProject(Project project) {
+		return projectDao.insertProject(project);	
+	}
+
+
+
+
+
+	public List<Project> fetchAllProjects() {
+		return projectDao.fetchAllProjects();
+	}
+
+
+
+
+
+	public Project fetchProjectById(Integer projectId) {
+		Optional<Project> op = projectDao.fetchProjectById(projectId);
+		
+		return projectDao.fetchProjectById(projectId).orElseThrow(() -> 
+		new NoSuchElementException("Project with ID= " + projectId + " does not exist."));
+		
+		
+			
+	}
+
+}

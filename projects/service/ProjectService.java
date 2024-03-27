66937a08e9de50f55c0f64bcb8c438bd05d758e1
sarahbethea <157849717+sarahbethea@users.noreply.github.com>
2024-03-26 21:53:55 -0600

    Week 11

--- projects/service/ProjectService.java
+++ projects/service/ProjectService.java
@@ -42,4 +42,29 @@ public class ProjectService {
 			
 	}
 
+
+
+
+
+	public void modifyProjectDetails(Project project) {
+		if(!projectDao.modifyProjectDetails(project)) {
+			throw new DbException("Project with ID= " 
+					+ project.getProjectId() + " does not exist.");
+
+		}
+		
+	}
+
+
+
+
+
+	public void deleteProject(Integer projectId) {
+		if(!projectDao.deleteProject(projectId)) {
+			throw new DbException("Project with ID=" + projectId + " does not exist.");
+			
+		}
+		
+	}
+
 }
